511ff2cf59e239979a2c384189bb81f9f61e4610
acer <dickiichsan86@gmail.com>
2024-09-13 23:50:56 +0700

    add-some-features

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
+function isValidURL(url) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(url);
+}
+
 function generateQRCode() {
   var input = document.getElementById("url-input").value;
-  if (input) {
-    var canvas = document.getElementById("qrcode");
-    QRCode.toCanvas(canvas, input, function (error) {
+  var size = document.getElementById("size").value;
+  var color = document.getElementById("color").value;
+  var errorMessage = document.getElementById("error-message");
+
+  // Validate URL
+  if (!isValidURL(input)) {
+    errorMessage.style.display = "block";
+    return;
+  } else {
+    errorMessage.style.display = "none";
+  }
+
+  var canvas = document.getElementById("qrcode");
+  QRCode.toCanvas(
+    canvas,
+    input,
+    {
+      width: parseInt(size),
+      color: {
+        dark: color, // Set QR Code color
+        light: "#ffffff", // Background color (white)
+      },
+    },
+    function (error) {
       if (error) console.error(error);
       console.log("QR Code generated successfully!");
       document.getElementById("download-btn").style.display = "block"; // Show download button after QR code is generated
-    });
-  }
+    }
+  );
 }
 
 function downloadQRCode() {
